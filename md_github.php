47b8f7f39b2a749fef085961f50c3be072a10022
nilsnolde <nilsnolde@gmail.com>
2019-01-05 13:18:56 +0300

    added shortcode for checkout and using API now instead of scraped HTML

--- md_github.php
+++ md_github.php
@@ -9,22 +9,37 @@
    */
 
 function md_github_handler($atts) {
-  //run function that actually does the work of the plugin
-  $md_output = md_github_function($atts);
+  //get API path from file URL
+  $json = get_api_path($atts, 'file')
+  $md_output = base64_decode($json['content'])
   //send back text to replace shortcode in post
   return $md_output;
 }
 
-function nbconvert_get_most_recent_git_change_for_file_from_api($url) {
+function md_github_checkout($atts) {
+  $last_update_htnl = get_github_checkout($atts, 'checkout')
+  return $last_update_htnl
+}
+
+function get_api_path($atts, $method) {
+
+  extract(shortcode_atts(array(
+          'url' => "",
+        ), $atts
+      )
+   );
 
   $url_list = explode('/', $url);
-
   $owner = $url_list[3];
   $repo = $url_list[4];
   $branch = $url_list[6];
   $path = implode("/", array_slice($url_list, 7));
 
-  $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/commits/'.$branch.'?path='. $path.'&page=1';
+  if ($method === 'file') {
+    $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/contents/'.$path.'?ref='.$branch.;
+  } else {
+    $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/commits/'.$branch.'?path='. $path.'&page=1';
+  }
 
   $context_params = array(
     'http' => array(
@@ -34,68 +49,35 @@ function nbconvert_get_most_recent_git_change_for_file_from_api($url) {
     )
   );
 
-
   $res = file_get_contents($request_url, FALSE, stream_context_create($context_params));
+  $json = json_decode($res, true);
 
-  $datetime = json_decode($res, true)['commit']['committer']['date'];
-
-  $max_datetime = strtotime($datetime);
-  $max_datetime_f = date('d/m/Y H:i:s', $max_datetime);
-
-  return $max_datetime_f;
+  return $json
 }
 
-function md_github_function($atts) {
-  //process plugin
-  extract(shortcode_atts(array(
-        'url' => "",
-     ), $atts));
+function get_github_checkout($json) {
 
-  $html = file_get_contents($url);
-  $nb_output = md_github_getHTMLByTagName('article', $html);
+  $last_update_date_time = $url['commit']['committer']['date'];
 
-  $last_update_date_time = nbconvert_get_most_recent_git_change_for_file_from_api($url);
-
-  $pulled_md = '<div class="markdown-github"
-    <div class="markdown-github-labels">
-      <label class="github-link">
-        <a href="'.$url.'" target="_blank">Check it out on github</a>
-        <label class="github-last-update"> Last updated: '.$last_update_date_time.'</label>
-      </label>
-    </div>
-    <article class="markdown-body>'.$nb_output.'
-    </article>
+  $checkout_label = '<div class="markdown-github"
+      <div class="markdown-github-labels">
+        <label class="github-link">
+          <a href="'.$url.'" target="_blank">Check it out on github</a>
+          <label class="github-last-update"> Last updated: '.$last_update_date_time.'</label>
+        </label>
+      </div>
     </div>';
 
-  //send back text to calling function
-  return $pulled_md;
-}
-
-function md_github_innerHTML(DOMNode $elm) {
-  $innerHTML = '';
-  $children  = $elm->childNodes;
-
-  foreach($children as $child) {
-    $innerHTML .= $elm->ownerDocument->saveHTML($child);
+  return $checkout_label
   }
 
-  return $innerHTML;
-}
-
-function md_github_getHTMLByTagName($name, $html) {
-    $dom = new DOMDocument;
-    libxml_use_internal_errors(true);
-    $dom->loadHTML($html);
-    $node = $dom->getElementsByTagName($name)->item(0);
-    if ($node) {
-        $inner_output = md_github_innerHTML($node);
-        return $inner_output;
-    }
-    return FALSE;
+function get_markdown_content($json) {
+  return
 }
 
 function md_github_enqueue_style() {
 	wp_enqueue_style( 'md-github', plugins_url( '/css/github-markdown.css', __FILE__ ));
 }
 add_action( 'wp_enqueue_scripts', 'md_github_enqueue_style' );
+add_shortcode('checkout_github', "md_github_checkout")
 add_shortcode("md_github", "md_github_handler");
